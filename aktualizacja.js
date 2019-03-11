7303a5dc822f3f8a86232208e4b68bfadaca6b74
noisuper <48448049+noisuper@users.noreply.github.com>
2019-03-11 17:57:00 +0100

    Update aktualizacja.js

--- aktualizacja.js
+++ aktualizacja.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.post(`/`, (req, res) => {
     console.log(req.body);
     code = execSync('git pull');
+    res.send('Zrobilem aktualizacje')
 
 })
 
