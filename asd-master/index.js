afdb892cc4dd989202f402dbc25b8b8aa88afece
noisuper <48448049+noisuper@users.noreply.github.com>
2019-03-13 23:26:46 +0100

    Update index.js

--- asd-master/index.js
+++ asd-master/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 var bodyParser = require('body-parser');
 
 const app = express()
-const port = 6969
+const port = 8080
 
 app.use(express.static('public'))
 app.use(bodyParser.json()); // for parsing application/json
