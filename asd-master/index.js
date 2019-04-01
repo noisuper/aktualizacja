e751ddbbce225e8b118921cbf9ef49ec2fa34cc5
7-15 <7-15@zentyal-cku.lan>
2019-04-01 16:59:46 +0200

    początki API REST - restowego

--- asd-master/index.js
+++ asd-master/index.js
@@ -9,38 +9,54 @@ app.use(express.static('public'))
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+var stan = false;
 var licznik = 0;
+
 app.get('/', (req, res) => {
-    res.send(`Liczę, i mam ${licznik}`);
-    licznik++;
+  res.send(`Liczę, i mam ${licznik}`);
+  licznik++;
 })
 
+
+
+app.get('/status', (req, res) => {
+    // res.send(`Liczę, i mam ${licznik}`);
+    // licznik++;
+    res.send({state:stan});
+})
 app.post('/lampka.html', (req, res) => {
     //console.log(req.body);
     res.redirect('lampka1.html');
-    execSync('echo "1" > /sys/class/gpio/gpio26/value');
+    stan = true;
+    console.log(stan);
+    //execSync('echo "1" > /sys/class/gpio/gpio26/value');
     //licznik++;
 })
 
 app.post('/lampka1.html', (req, res) => {
     //console.log(req.body);
     res.redirect('lampka.html');
-    execSync('echo "0" > /sys/class/gpio/gpio26/value');
+    stan = false;
+    console.log(stan);
+    JSON.stringify(stan);
+    //execSync('echo "0" > /sys/class/gpio/gpio26/value');
     //licznik++;
 })
 
 
- try {
-    execSync('echo "26" > /sys/class/gpio/export');
-    execSync('echo "out" > /sys/class/gpio/gpio26/direction');
 
-  } 
-  catch (error) {
+
+//  try {
+//     execSync('echo "26" > /sys/class/gpio/export');
+//     execSync('echo "out" > /sys/class/gpio/gpio26/direction');
+
+//   } 
+//   catch (error) {
 //     error.status;  // Might be 127 in your example.
 //     error.message; // Holds the message you typically want.
 //     error.stderr;  // Holds the stderr output. Use `.toString()`.
 //     error.stdout;  // Holds the stdout output. Use `.toString()`.
-  }
+  //}
 
 
 app.listen(port,
